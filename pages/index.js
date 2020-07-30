f18236947746c89603cdeb8e4ebbe234053fe30b
Daniel Nguyen <danngu@uw.edu>
2020-07-30 15:26:29 -0700

    animations

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Cases from '../components/cases';
 import { motion } from 'framer-motion';
 import Nav from '../components/nav';
 
-const Tag = styled.div`
+const Tag = styled(motion.div)`
 	width: 10rem;
 	height: 2rem;
 	border-radius: 0.25rem;
@@ -17,28 +17,46 @@ const Tag = styled.div`
 	color: ${({ theme }) => theme.colors.text};
 `;
 
+const container = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			staggerChildren: 0.8,
+			delayChildren: 0.6,
+			duration: 1.6,
+			easing: "EaseOut",
+		},
+	},
+};
+
+const item = {
+	hidden: { opacity: 0, y: 10 },
+	show: { opacity: 1, y: 0 },
+};
+
 export default function index() {
 	return (
 		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{
-				opacity: 1,
-				transition: { duration: 0.8, ease: 'easeOut' }}}
-			exit={{opacity: 0}}
-			>
+			variants={container}
+			initial='hidden'
+			animate='show'
+			exit='hidden'>
 			<Nav></Nav>
-			<h1>Hey, I'm Daniel. </h1>
-			<Tag>Product Designer</Tag>
-			<p>
+			<motion.h1 variants={item}>Hey, I'm Daniel. </motion.h1>
+			<Tag variants={item}>Product Designer</Tag>
+			<motion.p variants={item}>
 				I'm a young designer determined to design inclusive and
 				accessible products that positively impact the world.
-			</p>
-			<p>
+			</motion.p>
+			<motion.p variants={item}>
 				Right now, I am longboarding, educating myself on systematic
 				racism, and looking for full time jobs and internships.
-			</p>
-			<h2>Featured Works</h2>
-			<Cases></Cases>
+			</motion.p>
+			<motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0, transition: { delay: 4, duration: 0.8}}}>
+				<motion.h2>Featured Works</motion.h2>
+				<Cases></Cases>
+			</motion.div>
 		</motion.div>
 	);
 }
